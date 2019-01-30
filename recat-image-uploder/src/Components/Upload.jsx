dd8c03f655895559f6b2615fe072505b58b59401
FIRST_NAME LAST_NAME <sharmasaloni061@gmail.com>
2019-01-30 16:12:34 +0530

    File readers

--- recat-image-uploder/src/Components/Upload.jsx
+++ recat-image-uploder/src/Components/Upload.jsx
@@ -1,19 +1,42 @@
 import React from 'react'
 
 class Upload extends React.Component {
+    constructor(props){
+        super(props);
+        this.fileInput = React.createRef()
+        this.imgOutput = React.createRef()
+    }
+    // componentDidMount() {
+    //     console.log(this.fileInput.files);
+    // }
 
-    handleOnSubmit(event){
+    handleOnSubmit(event) {
         event.preventDefault();
-}
+    }
+    
+
+    handleOnchange(event){
+       console.log(event);
+       console.log(this.fileInput.current.files);
+       let reader = new FileReader()
+       reader.onload = (event)=>{
+                this.imgOutput.current.src = event.target.result
+                
+       }
+       reader.readAsDataURL(this.fileInput.current.files[0])
+
+
+    }
 
     render() {
-        
         return (
             <div>
-                <form onSubmit={(event)=>this.handleOnSubmit(event)}>
-                  <input type="file" name="myFile" multiple />
+            Upload Images
+                <form onSubmit={(event) => this.handleOnSubmit(event)}>
+                    <input ref={this.fileInput} onChange={(event)=>this.handleOnchange(event)} type="file" name="myFile" accept="image/png, image/jpeg ,image/jpg" multiple />
                 </form>
-               
+                <img ref={this.imgOutput} src= "#" alt="My Image"/>
+
             </div>
         )
     }
